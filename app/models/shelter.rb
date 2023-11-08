04669b9d7acab123966f40fb9358d4c19b953dd1
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-08 14:00:11 -0700

    Add validation message

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,5 +2,5 @@ class Shelter < ApplicationRecord
   has_many :cats
 
   validates :name, :city, presence: true
-  validates :name, uniqueness: {scope: :city}
+  validates :name, uniqueness: {scope: :city, message: "is taken by a shelter in that city"}
 end
