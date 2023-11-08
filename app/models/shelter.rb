5a4c80558689ec067353aa1e5ffd06048ec1dd53
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-08 13:47:56 -0700

    Add scope to validation

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,5 +2,5 @@ class Shelter < ApplicationRecord
   has_many :cats
 
   validates :name, :city, presence: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: {scope: :city}
 end
