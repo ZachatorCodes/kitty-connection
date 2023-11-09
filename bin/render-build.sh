76e12e0689e508c47ea4190cbfd9d3b57ccd9f90
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-08 17:10:43 -0700

    Remove reset migration

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,7 +9,10 @@ cp -a client/build/. public/
 
 # Build commands for back end
 bundle install
-bundle exec rake db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rake db:migrate
 
 # if you have seed data, run this command for the initial deploy only to avoid duplicate records
 # bundle exec rake db:seed 
+
+# for destructive behavior
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1
