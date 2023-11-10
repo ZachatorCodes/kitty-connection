489cb2bd5ab0b6afe42805d654b69914294c5e88
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-10 12:11:51 -0700

    Add error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,30 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  before_action :authorize
+  skip_before_action :authorize, only: :hello_world
+
+  # Test to confirm sessions and cookies are working
   def hello_world
     session[:count] = (session[:count] || 0) + 1
-    render json: { count: session[:count] }
+    render json: {count: session[:count]}
+  end
+
+  private
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    render json: {errors: ["Not authorized"]}, status: :unauthorized unless @current_user
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: {error: "Not Found"}, status: :not_found
   end
 end
