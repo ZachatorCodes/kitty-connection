858b9b56213c47822d7505d2463afcb13ecb0fe0
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-10 13:18:39 -0700

    Add cat index route

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,8 @@ Rails.application.routes.draw do
   get "/me", to: "users#show"
   post "/signup", to: "users#create"
 
+  # Cat routes
+  resources :cats, only: [:index]
+
   get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
