605f695fb29561195065d63e9eb0164ee3cda898
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-15 14:36:28 -0700

    Add logic to check for user

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/user";
+import Navbar from "./Navbar";
 
 function Home({ cats }) {
-  return (
-    <div className="home">
-      <div className="welcome">
-        <h1>Welcome to Kitty Connection</h1>
+  const { user } = useContext(UserContext);
+  if (user) {
+    return (
+      <div className="home">
+        <Navbar />
+        <div className="welcome">
+          <h1>Welcome to Kitty Connection, {user.first_name}</h1>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="home">
+        <Navbar />
+        <div className="welcome">
+          <h1>Welcome to Kitty Connection</h1>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default Home;
