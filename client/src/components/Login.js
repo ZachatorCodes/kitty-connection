ce5374850becc574e7f433d9698ca7e2fb3feeec
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-15 13:20:01 -0700

    Create basic log-in form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,18 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { Card, Divider } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { Button, Card } from "@mui/material";
 
 function Login() {
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    console.log(`NAME: ${e.target.name} | VALUE: ${e.target.value}`);
+
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setUserInfo({
+      ...userInfo,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(
+      "Form Submitted!", "\n", userInfo
+    );
+  }
+
   return (
-    <div className="login">
-      <Box>
-        <Card>
-          <TextField required variant="filled" label="Username" sx={{margin: "10px"}}></TextField>
-          <Divider/>
-          <TextField required variant="filled" label="Password" sx={{margin: "10px"}}></TextField>
-        </Card>
-      </Box>
+    <div className="login" align="center">
+      <Card className="login-form">
+        <Typography variant="h4">Log In</Typography>
+        <Box component="form" onSubmit={handleSubmit}>
+          <TextField
+            required
+            variant="filled"
+            name="username"
+            label="Username"
+            value={userInfo.username}
+            onChange={handleChange}
+            sx={{ margin: "10px" }}
+          ></TextField>
+          <br />
+          <TextField
+            required
+            variant="filled"
+            name="password"
+            label="Password"
+            value={userInfo.password}
+            onChange={handleChange}
+            sx={{ margin: "10px" }}
+          ></TextField>
+          <br />
+          <Button type="submit">Log In</Button>
+        </Box>
+      </Card>
     </div>
   );
 }
