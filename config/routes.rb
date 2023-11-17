ea80c6cc1bb8de3b47b890737e2124dd9906ba3e
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-16 17:51:47 -0700

    Add destroy resource for cats

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
 
   # Cat routes
-  resources :cats, only: [:index]
+  resources :cats, only: [:index, :destroy]
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
