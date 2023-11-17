3258c16e689dd8bf1ad118454aeee6931847e3a8
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-17 13:23:47 -0700

    Add collapsible content (and reduce package size)

--- client/src/components/CatInfo.js
+++ client/src/components/CatInfo.js
@@ -1,12 +1,39 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "./Navbar";
-import { Card, CardHeader, Container, Paper, Typography } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+import Collapse from "@mui/material/Collapse";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 function CatInfo({ cats }) {
   const { id: catID } = useParams();
   const selectedCat = cats.find((cat) => cat.id === parseInt(catID));
-  
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   console.log(selectedCat);
 
   if (selectedCat) {
@@ -22,7 +49,26 @@ function CatInfo({ cats }) {
         </Container>
         <Container maxWidth="sm">
           <Card>
-            <CardHeader title={selectedCat.name} />
+            <CardHeader title={selectedCat.name} subheader={selectedCat.shelter.name}/>
+            <CardContent>
+              <Typography>Age: {selectedCat.age}</Typography>
+              <Typography>Sex: {selectedCat.sex === 1 ? "Male" : "Female"}</Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <ExpandMore
+                expand={expanded}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography>Hello!</Typography>
+              </CardContent>
+            </Collapse>
           </Card>
         </Container>
       </div>
