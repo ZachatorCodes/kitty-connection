31e7f34a7fe344da9cebf2a650a007d32ec10835
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-16 18:19:31 -0700

    Style welcome message

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { UserContext } from "../context/user";
 import Navbar from "./Navbar";
-import { Card, Paper } from "@mui/material";
+import { Container, Paper, Typography } from "@mui/material";
 import Cats from "./Cats";
 
 function Home({ cats, onDeleteCat }) {
@@ -11,10 +11,12 @@ function Home({ cats, onDeleteCat }) {
       <div className="home">
         <Navbar />
         <div className="welcome">
-          <Paper>
-            <h1>Welcome to Kitty Connection, {user.first_name}</h1>
-          </Paper>
-          <Cats cats={cats} onDeleteCat={onDeleteCat}/>
+          <Container maxWidth="lg">
+            <Paper elevation={3}>
+              <Typography padding="3px" margin="24px" align="center" variant="h3">Welcome to Kitty Connection, {user.first_name}</Typography>
+            </Paper>
+          </Container>
+          <Cats cats={cats} onDeleteCat={onDeleteCat} />
         </div>
       </div>
     );
@@ -23,7 +25,12 @@ function Home({ cats, onDeleteCat }) {
       <div className="home">
         <Navbar />
         <div className="welcome">
-          <h1>Welcome to Kitty Connection</h1>
+          <Container maxWidth="lg">
+            <Paper elevation={3}>
+              <Typography padding="3px" margin="24px" align="center" variant="h3">Welcome to Kitty Connection</Typography>
+            </Paper>
+          </Container>
+          <Cats cats={cats} />
         </div>
       </div>
     );
