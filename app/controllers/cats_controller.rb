22c45261ab24b8c820ab031a737ee4592fd39487
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-17 15:02:03 -0700

    Create update action

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -6,9 +6,21 @@ class CatsController < ApplicationController
     render json: cats, status: :ok
   end
 
+  def update
+    cat = Cat.find(params[:id])
+    cat.update!(update_cat_params)
+    render json: cat, status: :ok
+  end
+
   def destroy
     cat = Cat.find(params[:id])
     cat.destroy
     head :no_content
+  end
+
+  private
+
+  def update_cat_params
+    params.permit(:name, :age, :sex)
   end
 end
