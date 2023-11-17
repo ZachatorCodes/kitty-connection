aaf4744a810a9f1adea1d063aee6e8c551be3866
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-16 17:53:33 -0700

    Drill cat deletion handling

--- client/src/components/Cats.js
+++ client/src/components/Cats.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Grid } from "@mui/material";
 import BuildCat from "./BuildCat";
 
-function Cats({ cats }) {
+function Cats({ cats, onDeleteCat }) {
   return (
     <div className="cats">
       <Grid container spacing={1}>
         {cats.map((cat) => (
-          <BuildCat cat={cat} />
+          <BuildCat cat={cat} onDeleteCat={onDeleteCat}/>
         ))}
       </Grid>
     </div>
