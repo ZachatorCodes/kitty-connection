d400a7b8d2a63895bd1b8e7f4a5d92a98dcde836
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-19 15:29:11 -0700

    Build out component infomation

--- client/src/components/ShelterInfo.js
+++ client/src/components/ShelterInfo.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import Navbar from "./Navbar";
+import { Container, Paper, Typography } from "@mui/material";
 
-function ShelterInfo() {
+function ShelterInfo({ shelters }) {
+  const { id: shelterID } = useParams();
+  
+  const selectedShelter = shelters.find(
+    (shelter) => shelter.id === parseInt(shelterID)
+  );
+
   return (
-    <div>ShelterInfo</div>
-  )
+    <div className="shelter-page">
+      <Navbar />
+      <div className="shelter-info">
+        <Container maxWidth="lg">
+          <Paper elevation={3}>
+            <Typography padding="3px" margin="24px" align="center" variant="h3">
+              Shelter Information
+            </Typography>
+          </Paper>
+        </Container>
+      </div>
+    </div>
+  );
 }
 
-export default ShelterInfo
+export default ShelterInfo;
