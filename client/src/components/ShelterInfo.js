9c3a01e84d7f9f7f7f08b8af3948f37a80c98c57
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-19 15:53:58 -0700

    Add error handling for no shelter and no cats

--- client/src/components/ShelterInfo.js
+++ client/src/components/ShelterInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "./Navbar";
 import { Card, CardHeader, Container, Paper, Typography } from "@mui/material";
+import Cats from "./Cats";
 
 function ShelterInfo({ shelters }) {
   const { id: shelterID } = useParams();
@@ -10,25 +11,80 @@ function ShelterInfo({ shelters }) {
     (shelter) => shelter.id === parseInt(shelterID)
   );
 
-  return (
-    <div className="shelter-page">
-      <Navbar />
-      <div className="shelter-info">
+  console.log(selectedShelter);
+
+  if (selectedShelter) {
+    return (
+      <div className="shelter-page">
+        <Navbar />
+        <div className="shelter-info">
+          <Container maxWidth="lg">
+            <Paper elevation={3}>
+              <Typography
+                padding="3px"
+                margin="24px"
+                align="center"
+                variant="h3"
+              >
+                Shelter Information
+              </Typography>
+            </Paper>
+          </Container>
+          <Container maxWidth="sm">
+            <Card>
+              <CardHeader
+                title={selectedShelter.name}
+                subheader={`Located In: ${selectedShelter.city}`}
+              />
+            </Card>
+          </Container>
+        </div>
         <Container maxWidth="lg">
           <Paper elevation={3}>
             <Typography padding="3px" margin="24px" align="center" variant="h3">
-              Shelter Information
+              Adoptable Cats
             </Typography>
           </Paper>
         </Container>
-        <Container maxWidth="sm">
-          <Card>
-            <CardHeader title={selectedShelter.name} subheader={`Located In: ${selectedShelter.city}`} />
-          </Card>
+        <Container>
+          {selectedShelter.cats ? (
+            <Cats cats={selectedShelter.cats} />
+          ) : (
+            <Container maxWidth="sm">
+              <Card align="center">
+                <CardHeader title="No Cats Available At This Shelter. Sorry!" />
+              </Card>
+            </Container>
+          )}
         </Container>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="shelter-page">
+        <Navbar />
+        <div className="shelter-info">
+          <Container maxWidth="lg">
+            <Paper elevation={3}>
+              <Typography
+                padding="3px"
+                margin="24px"
+                align="center"
+                variant="h3"
+              >
+                Shelter Information
+              </Typography>
+            </Paper>
+          </Container>
+          <Container maxWidth="sm">
+            <Card>
+              <CardHeader title="That shelter doesn't exist. Sorry!" />
+            </Card>
+          </Container>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ShelterInfo;
