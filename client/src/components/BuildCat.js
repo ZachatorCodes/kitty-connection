74adfa633ad0a99e5de3b678c3e38d405acf0e70
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-19 15:53:39 -0700

    Add error handling for if cat doesn't have shelter

--- client/src/components/BuildCat.js
+++ client/src/components/BuildCat.js
@@ -21,19 +21,35 @@ function BuildCat({ cat }) {
     navigate(`/cats/${cat.id}`);
   }
 
-  return (
-    <Grid item xs={12} sm={6} md={4} lg={3}>
-      <Card className="cat-card" elevation={5}>
-        <CardActionArea onClick={redirectToCat}>
-          <CardHeader title={cat.name} subheader={cat.shelter.name} />
-          <CardContent>
-            <Typography>Age: {cat.age}</Typography>
-            <Typography>Sex: {cat.sex === 1 ? "Male" : "Female"}</Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Grid>
-  );
+  if (cat.shelter) {
+    return (
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Card className="cat-card" elevation={5}>
+          <CardActionArea onClick={redirectToCat}>
+            <CardHeader title={cat.name} subheader={cat.shelter.name} />
+            <CardContent>
+              <Typography>Age: {cat.age}</Typography>
+              <Typography>Sex: {cat.sex === 1 ? "Male" : "Female"}</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    );
+  } else {
+    return (
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Card className="cat-card" elevation={5}>
+          <CardActionArea onClick={redirectToCat}>
+            <CardHeader title={cat.name} />
+            <CardContent>
+              <Typography>Age: {cat.age}</Typography>
+              <Typography>Sex: {cat.sex === 1 ? "Male" : "Female"}</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    );
+  }
 }
 
 export default BuildCat;
