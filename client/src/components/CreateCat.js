081960b1cddd5a4a140778942d4a9e9499e2db76
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-19 16:06:47 -0700

    Create basic component with handling for user

--- client/src/components/CreateCat.js
+++ client/src/components/CreateCat.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useContext } from "react";
+import Navbar from "./Navbar";
+import { Card, CardHeader, Container, Paper, Typography } from "@mui/material";
+import { UserContext } from "../context/user";
 
-function CreateCat() {
-  return (
-    <div>CreateCat</div>
-  )
+function CreateCat({ shelters }) {
+  const { loggedIn } = useContext(UserContext);
+  if (loggedIn) {
+    return (
+      <div className="create-cat">
+        <Navbar />
+        <div className="create-cat-info">
+          <Container maxWidth="lg">
+            <Paper elevation={3}>
+              <Typography
+                padding="3px"
+                margin="24px"
+                align="center"
+                variant="h3"
+              >
+                Add A Cat
+              </Typography>
+            </Paper>
+          </Container>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="create-cat">
+        <Navbar />
+        <div className="create-cat-info">
+          <Container maxWidth="lg">
+            <Paper elevation={3}>
+              <Typography
+                padding="3px"
+                margin="24px"
+                align="center"
+                variant="h3"
+              >
+                Add A Cat
+              </Typography>
+            </Paper>
+          </Container>
+          <Container maxWidth="sm">
+            <Card>
+              <CardHeader title="Please Log In To Add A Cat" />
+            </Card>
+          </Container>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default CreateCat
+export default CreateCat;
