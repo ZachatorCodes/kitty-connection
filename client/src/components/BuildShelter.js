28428b6c927d13c8f9084c42d31b0ace596fca1e
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-19 15:17:53 -0700

    Add click to navigate to shelter info

--- client/src/components/BuildShelter.js
+++ client/src/components/BuildShelter.js
@@ -1,11 +1,18 @@
 import { Card, CardActionArea, CardHeader, Grid } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function BuildShelter({ shelter }) {
+  const navigate = useNavigate();
+
+  function redirectToShelter() {
+    navigate(`/shelters/${shelter.id}`)
+  }
+
   return (
     <Grid item xs={12} sm={6} md={4}>
       <Card className="shelter-card" elevation={5}>
-        <CardActionArea>
+        <CardActionArea onClick={redirectToShelter}>
           <CardHeader title={shelter.name} subheader={shelter.city} />
         </CardActionArea>
       </Card>
