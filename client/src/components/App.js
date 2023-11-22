6126173d64b5f709c506d984d3dbd9a4f06a6219
Zach <zachatorcodes@gmail.com>
2023-11-21 18:28:29 -0700

    Wrap app in required providers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,44 +10,52 @@ import Profile from "./Profile";
 import ShelterPage from "./ShelterPage";
 import ShelterInfo from "./ShelterInfo";
 import CreateCat from "./CreateCat";
+import { CatsProvider } from "../context/cats";
+import { SheltersProvider } from "../context/shelters";
 
 function App() {
   return (
     <div className="app">
       <UserProvider>
-        <Routes>
-          <Route
-            exact
-            path="/cats/:id"
-            element={
-              <CatInfo
-                cats={cats}
-                onUpdateCat={handleUpdateCat}
-                onDeleteCat={handleDeleteCat}
+        <CatsProvider>
+          <SheltersProvider>
+            <Routes>
+              <Route
+                exact
+                path="/cats/:id"
+                element={
+                  <CatInfo
+                    cats={cats}
+                    onUpdateCat={handleUpdateCat}
+                    onDeleteCat={handleDeleteCat}
+                  />
+                }
               />
-            }
-          />
-          <Route
-            exact
-            path="/shelters"
-            element={<ShelterPage shelters={shelters} />}
-          />
-          <Route
-            exact
-            path="/shelters/:id"
-            element={<ShelterInfo shelters={shelters} />}
-          />
-          <Route
-            exact
-            path="/cats/new"
-            element={<CreateCat shelters={shelters} onAddCat={handleAddCat} />}
-          />
-          <Route exact path="/profile" element={<Profile />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/cats" element={<Home cats={cats} />} />
-          <Route exact path="/" element={<Home cats={cats} />} />
-        </Routes>
+              <Route
+                exact
+                path="/shelters"
+                element={<ShelterPage shelters={shelters} />}
+              />
+              <Route
+                exact
+                path="/shelters/:id"
+                element={<ShelterInfo shelters={shelters} />}
+              />
+              <Route
+                exact
+                path="/cats/new"
+                element={
+                  <CreateCat shelters={shelters} onAddCat={handleAddCat} />
+                }
+              />
+              <Route exact path="/profile" element={<Profile />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+              <Route exact path="/cats" element={<Home cats={cats} />} />
+              <Route exact path="/" element={<Home cats={cats} />} />
+            </Routes>
+          </SheltersProvider>
+        </CatsProvider>
       </UserProvider>
     </div>
   );
