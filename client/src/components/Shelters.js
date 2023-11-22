9125f78d522d4183247ec29797495da6c35678a4
Zach <zachatorcodes@gmail.com>
2023-11-21 19:03:21 -0700

    Change shelter data from prop to context

--- client/src/components/Shelters.js
+++ client/src/components/Shelters.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Grid } from "@mui/material";
 import BuildShelter from "./BuildShelter";
+import { SheltersContext } from "../context/shelters";
 
-function Shelters({ shelters }) {
+function Shelters() {
+  const { shelters } = useContext(SheltersContext);
+
   return (
     <div className="shelters">
       <Grid container spacing={2}>
         {shelters.map((shelter, index) => (
-          <BuildShelter shelter={shelter} key={index}/>
+          <BuildShelter shelter={shelter} key={index} />
         ))}
       </Grid>
     </div>
