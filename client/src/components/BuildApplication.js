b6047ccbfe7653909d5b1ced455052fa978c741c
Zach <zachatorcodes@gmail.com>
2023-11-23 16:03:01 -0700

    Build out application card

--- client/src/components/BuildApplication.js
+++ client/src/components/BuildApplication.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Card, CardHeader, Grid, IconButton } from "@mui/material";
+import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-function BuildApplication() {
-  return (
-    <div>BuildApplication</div>
-  )
+function BuildApplication({ application }) {
+  function handleApplicationState() {}
+
+  function handleDelete() {
+    fetch(`/applications/${application.id}`, {
+      method: "DELETE",
+    }).then((r) => {
+      if (r.ok) {
+        handleApplicationState(application);
+      }
+    });
+  }
+
+  if (application) {
+    return (
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Card className="application-card" elevation={5}>
+          <CardHeader
+            title={application.cat_name}
+            subheader={application.shelter_name}
+            action={
+              <IconButton onClick={handleDelete}>
+                <DeleteIcon />
+              </IconButton>
+            }
+          />
+        </Card>
+      </Grid>
+    );
+  }
 }
 
-export default BuildApplication
+export default BuildApplication;
