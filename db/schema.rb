668aa185f12889910679e8a6b4c3625c9cb9f381
Zach <zachatorcodes@gmail.com>
2023-11-23 15:26:45 -0700

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_23_170829) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_23_222247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_23_170829) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "cat_name"
+    t.string "shelter_name"
     t.index ["cat_id"], name: "index_applications_on_cat_id"
     t.index ["user_id"], name: "index_applications_on_user_id"
   end
