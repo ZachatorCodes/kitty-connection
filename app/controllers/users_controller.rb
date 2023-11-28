fd49a7c729712b22eab043d3d40a9cb55ef0f2ce
Zach Schwartz <zachatorcodes@gmail.com>
2023-11-28 15:10:49 -0700

    Formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
     user = User.create!(user_params)
     session[:user_id] = user.id
     render json: user, status: :created
-    
   end
 
   def show
