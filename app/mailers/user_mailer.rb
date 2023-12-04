3bcb94ee99cf54973084c9b9d683f5b097a544b7
Zach Schwartz <zachatorcodes@gmail.com>
2023-12-04 15:55:28 -0700

    Mail to email that user signed up with

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @login_url = 'https://kitty-connection.onrender.com/login'
-    mail(to: 'zschwartz2020@gmail.com', subject: "Welcome to Kitty Connection!")
+    mail(to: @user.email, subject: "Welcome to Kitty Connection!")
   end
 end
