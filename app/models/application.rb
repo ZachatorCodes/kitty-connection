d1ee75c083e5826204744137bcee9d81fadd7abd
Zach Schwartz <zachatorcodes@gmail.com>
2023-12-12 13:32:07 -0700

    Remove validation

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,6 +2,5 @@ class Application < ApplicationRecord
   belongs_to :user
   belongs_to :cat
 
-  validates :user_id, :cat_id, :shelter_name, :cat_name, presence: true
   validates :user_id, uniqueness: {scope: :cat_id, message: "can only have one active application per cat"}
 end
