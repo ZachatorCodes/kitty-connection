ef7e11cd9333a697474950ee1873da65b4d2869d
Zach Schwartz <zachatorcodes@gmail.com>
2023-12-12 13:36:35 -0700

    Include shelter name and cat name in serializer

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,3 +1,14 @@
 class ApplicationSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :cat_id, :cat_name, :shelter_name
+  attributes :id, :user_id, :cat_id, :shelter_name, :cat_name
+
+  belongs_to :shelter
+  belongs_to :cat
+
+  def shelter_name
+    object.shelter.name
+  end
+
+  def cat_name
+    object.cat.name
+  end
 end
