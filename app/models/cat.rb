fd7ecb0d54ac6a2ac8014672d690680b0e23542e
Zach Schwartz <zachatorcodes@gmail.com>
2023-12-12 13:32:19 -0700

    Increase cat name length

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -4,6 +4,6 @@ class Cat < ApplicationRecord
   has_many :users, through: :applications
 
   validates :name, :age, :sex, :shelter_id, presence: true
-  validates :name, length: {in: 3..20}
+  validates :name, length: {in: 3..30}
   validates :age, numericality: {only_integer: true, less_than_or_equal_to: 40, greater_than_or_equal_to: 0}
 end
