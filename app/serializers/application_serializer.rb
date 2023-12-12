479497b8aa52c8856f5e9f2c3ccb84772d5ecf4d
Zach Schwartz <zachatorcodes@gmail.com>
2023-12-12 13:41:56 -0700

    Fix object for shelter name

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,11 +1,10 @@
 class ApplicationSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :cat_id, :shelter_name, :cat_name
 
-  belongs_to :shelter
-  belongs_to :cat
+ 
 
   def shelter_name
-    object.shelter.name
+    object.cat.shelter.name
   end
 
   def cat_name
